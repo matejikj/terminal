26988b50d2776bf751ae2099f3bd23245a75aeaa
matejikj <jakub.matejik@centrum.cz>
2025-12-13 15:46:34 +0100

    #23: Added flow components to package.

--- core/src/main/java/com/matejik/terminal/config/TerminalAppShell.java
+++ core/src/main/java/com/matejik/terminal/config/TerminalAppShell.java
@@ -1,11 +1,20 @@
 package com.matejik.terminal.config;
 
 import com.matejik.terminal.ui.view.TerminalLayout;
+import com.vaadin.flow.component.dependency.Uses;
+import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.component.select.Select;
+import com.vaadin.flow.component.textfield.PasswordField;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.server.LoadDependenciesOnStartup;
 import com.vaadin.flow.theme.Theme;
 
 @Theme(value = "terminal")
 @LoadDependenciesOnStartup(TerminalLayout.class)
+@Uses(TextField.class)
+@Uses(PasswordField.class)
+@Uses(FormLayout.class)
+@Uses(Select.class)
 public class TerminalAppShell implements AppShellConfigurator {
 }
--- skoda/src/main/java/com/matejik/skoda/SkodaApplication.java
+++ skoda/src/main/java/com/matejik/skoda/SkodaApplication.java
@@ -1,10 +1,14 @@
 package com.matejik.skoda;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.matejik.skoda", "com.matejik.terminal"})
+@Theme(value = "terminal")
 public class SkodaApplication implements AppShellConfigurator {
 
   public static void main(String[] args) {
